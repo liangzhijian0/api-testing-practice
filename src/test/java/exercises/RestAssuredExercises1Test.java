43eeb5b3935e29b3aea38cfe773ca1a7d1fb6563
Ocean Liang <657175454@qq.com>
2018-07-30 10:40:20 +0800

    check_response_code_for_correct_request

--- src/test/java/exercises/RestAssuredExercises1Test.java
+++ src/test/java/exercises/RestAssuredExercises1Test.java
@@ -32,8 +32,10 @@ public class RestAssuredExercises1Test {
 
         given().
                 spec(requestSpec).
-                when().
-                then();
+                when().log().all().
+                get("/2016/drivers.json").
+                then().
+                statusCode(200);
     }
 
     /*******************************************************
