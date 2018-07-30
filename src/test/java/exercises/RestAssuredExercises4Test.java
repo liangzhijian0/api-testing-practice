c5a4a556e4b161e640d955b729c091f88cc5f158
Ocean Liang <657175454@qq.com>
2018-07-30 21:03:20 +0800

    checkResponseTimeFor2014CircuitList

--- src/test/java/exercises/RestAssuredExercises4Test.java
+++ src/test/java/exercises/RestAssuredExercises4Test.java
@@ -5,8 +5,11 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 
 public class RestAssuredExercises4Test {
 
@@ -90,6 +93,9 @@ public class RestAssuredExercises4Test {
         given().
                 spec(requestSpec).
                 when().
-                then();
+                get("/2014/circuits.json").
+                then().
+                assertThat().
+                time(lessThan(100L),TimeUnit.MILLISECONDS);
     }
 }
